f4991bf8053e1137529e771870f88b36b1312f96
hector2430 <70005501+hector2430@users.noreply.github.com>
2024-01-08 17:16:00 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,15 @@
+
 <?php
-$servername = "roundhouse.proxy.rlwy.net";
+$servername = "roundhouse.proxy.rlwy.net:44149";
 $username = "root";
 $password = "";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=agilapp", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully";
 ?>
