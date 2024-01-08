efc844cc5f0147931edac0cf2aed5bd47c62e2b8
hector2430 <70005501+hector2430@users.noreply.github.com>
2024-01-08 16:51:12 -0300

    Update usuarioAjax.php

--- app/ajax/usuarioAjax.php
+++ app/ajax/usuarioAjax.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once '../../config/app.php';
-require_once '../views/inc/session_start.php';
+require_once '../Views/inc/session_start.php';
 require_once '../../autoload.php';
 
 use app\Controllers\usuarioController;
