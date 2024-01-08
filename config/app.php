2e69581277732532a2f1bbca4c37ac092f449d85
hector2430 <70005501+hector2430@users.noreply.github.com>
2024-01-08 16:38:14 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 const APP_URL = "https://agilprueba-production.up.railway.app/";
 const APP_NAME ="AGIL SISTEMA WEB";
-const APP_SESSION_NAME "AGIL";
+const APP_SESSION_NAME ="AGIL";
 
 date_default_timezone_set("America/Santiago");
 ?>
