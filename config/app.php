25ce0d3fa21249ae5a2414db4d95a080dffe9e7b
hector2430 <70005501+hector2430@users.noreply.github.com>
2024-01-08 16:29:53 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -1,8 +1,8 @@
 <?php 
 
-const $APP_URL = $_ENV["APP_URL"];
-const $APP_NAME = $_ENV["APP_NAME"];
-const $APP_SESSION_NAME =$_ENV["APP_SESSION_NAME"];
+const APP_URL = $_ENV["APP_URL"];
+const APP_NAME = $_ENV["APP_NAME"];
+const APP_SESSION_NAME =$_ENV["APP_SESSION_NAME"];
 
 date_default_timezone_set("America/Santiago");
 ?>
