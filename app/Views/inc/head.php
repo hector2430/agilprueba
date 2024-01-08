85988b584c3c98988d7f013114db91a496f123e5
hector2430 <70005501+hector2430@users.noreply.github.com>
2024-01-08 16:49:17 -0300

    Update head.php

--- app/Views/inc/head.php
+++ app/Views/inc/head.php
@@ -4,4 +4,4 @@
 <link rel="stylesheet" href="<?php echo APP_URL;  ?>/app/views/css/bulma.min.css">
 <link rel="stylesheet" href="<?php echo APP_URL;  ?>/app/views/css/estilos.css">
 <link rel="stylesheet" href="<?php echo APP_URL;  ?>/app/views/css/sweetalert2.min.css">
-<script src="<?php echo APP_URL;  ?>/app/views/js/sweetalert2.all.min.js"></script>
+<script src="<?php echo APP_URL;  ?>/app/Views/js/sweetalert2.all.min.js"></script>
