2d78d0f2b1f736e51bc3dfce807a3eb371b3013d
hector2430 <70005501+hector2430@users.noreply.github.com>
2024-01-08 17:13:27 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "roundhouse.proxy.rlwy.net";
 $username = "root";
-$password = "2EaE5hB4F1CebaE62-34C44b1bC2d1fA";
+$password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
